5fbc1d480dbc8bcbe1d800c1f1f7ff18d358da2b
Otávio Ribeiro Filho <otavio.r.filho@gmail.com>
2013-12-05 09:40:02 -0300

    Commit com agente sendo iniciado e identificado corretamente

--- src/SlidingSolverAgent.java
+++ src/SlidingSolverAgent.java
@@ -2,5 +2,9 @@ import jade.core.AID;
 import jade.core.Agent;
 
 public class SlidingSolverAgent extends Agent{
-
+	
+	protected void setup(){
+		System.out.println("Meu nome e: " + getAID().getName());
+	}
+	
 }
